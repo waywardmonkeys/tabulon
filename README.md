03dc26011405d92a09cc099f8c3da16e9d650d8e
Bruce Mitchener <bruce.mitchener@gmail.com>
2025-03-04 23:28:35 +0700

    Use Rust 1.85 as the MSRV
    By the time this is released, this won't be the most recent version
    and it'll let us switch to 2024 edition.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ The Tabulon library provides functionality for working with canvas-like scenes.
 
 ## Minimum supported Rust Version (MSRV)
 
-This version of Tabulon has been verified to compile with **Rust 1.82** and later.
+This version of Tabulon has been verified to compile with **Rust 1.85** and later.
 
 Future versions of Tabulon might increase the Rust version requirement.
 It will not be treated as a breaking change and as such can even happen with small patch releases.
--- tabulon/src/floatfuncs.rs
+++ tabulon/src/floatfuncs.rs
@@ -1,14 +1,6 @@
 // Copyright 2024 the Tabulon Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// In Rust 1.84 (https://github.com/rust-lang/rust/pull/131304), `abs` and
-// `copysign` were added to `core`, so we no longer need these forwarded to
-// libm.
-#![cfg_attr(
-    not(feature = "std"),
-    allow(dead_code, reason = "abs and copysign were added to core in 1.84")
-)]
-
 //! Shims for math functions that ordinarily come from std.
 
 /// Defines a trait that chooses between libstd or libm implementations of float methods.
@@ -23,6 +15,7 @@ macro_rules! define_float_funcs {
         /// the `std` feature is not enabled.
         ///
         /// For documentation see the respective functions in the std library.
+        #[expect(dead_code, reason = "This is here for future use, isn't used yet.")]
         #[cfg(not(feature = "std"))]
         pub(crate) trait FloatFuncs : Sized {
             $(fn $name(self $(,$arg: $arg_ty)*) -> $ret;)+
@@ -43,13 +36,9 @@ macro_rules! define_float_funcs {
 }
 
 define_float_funcs! {
-    // This is not needed once the MSRV is 1.84 or later.
-    fn abs(self) -> Self => fabsf;
     fn atan2(self, other: Self) -> Self => atan2f;
     fn cbrt(self) -> Self => cbrtf;
     fn ceil(self) -> Self => ceilf;
-    // This is not needed once the MSRV is 1.84 or later.
-    fn copysign(self, sign: Self) -> Self => copysignf;
     fn floor(self) -> Self => floorf;
     fn hypot(self, other: Self) -> Self => hypotf;
     // Note: powi is missing because its libm implementation is not efficient
