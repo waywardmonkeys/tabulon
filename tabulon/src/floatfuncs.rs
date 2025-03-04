cec31cf3c81c7583a99cb82e1c2769e3d61190f5
Bruce Mitchener <bruce.mitchener@gmail.com>
2025-03-05 00:09:34 +0700

    FloatFuncs for `f64`

--- tabulon/src/floatfuncs.rs
+++ tabulon/src/floatfuncs.rs
@@ -7,7 +7,7 @@
 macro_rules! define_float_funcs {
     ($(
         fn $name:ident(self $(,$arg:ident: $arg_ty:ty)*) -> $ret:ty
-        => $lfname:ident;
+        => $lname:ident/$lfname:ident;
     )+) => {
 
         /// Since core doesn't depend upon libm, this provides libm implementations
@@ -32,18 +32,28 @@ macro_rules! define_float_funcs {
             })+
         }
 
+        #[cfg(not(feature = "std"))]
+        impl FloatFuncs for f64 {
+            $(fn $name(self $(,$arg: $arg_ty)*) -> $ret {
+                #[cfg(feature = "libm")]
+                return libm::$lname(self $(,$arg)*);
+
+                #[cfg(not(feature = "libm"))]
+                compile_error!("tabulon requires either the `std` or `libm` feature")
+            })+
+        }
     }
 }
 
 define_float_funcs! {
-    fn atan2(self, other: Self) -> Self => atan2f;
-    fn cbrt(self) -> Self => cbrtf;
-    fn ceil(self) -> Self => ceilf;
-    fn floor(self) -> Self => floorf;
-    fn hypot(self, other: Self) -> Self => hypotf;
+    fn atan2(self, other: Self) -> Self => atan2/atan2f;
+    fn cbrt(self) -> Self => cbrt/cbrtf;
+    fn ceil(self) -> Self => ceil/ceilf;
+    fn floor(self) -> Self => floor/floorf;
+    fn hypot(self, other: Self) -> Self => hypot/hypotf;
     // Note: powi is missing because its libm implementation is not efficient
-    fn powf(self, n: Self) -> Self => powf;
-    fn round(self) -> Self => roundf;
-    fn sin_cos(self) -> (Self, Self) => sincosf;
-    fn sqrt(self) -> Self => sqrtf;
+    fn powf(self, n: Self) -> Self => pow/powf;
+    fn round(self) -> Self => round/roundf;
+    fn sin_cos(self) -> (Self, Self) => sincos/sincosf;
+    fn sqrt(self) -> Self => sqrt/sqrtf;
 }
